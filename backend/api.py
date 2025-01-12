84a19ff382c79e23496b7ada2e10d239a7412a81
Natasha Taylor <natasha.ca.taylor@gmail.com>
2025-01-12 08:56:21 -0500

    make user 2 json

--- backend/api.py
+++ backend/api.py
@@ -12,18 +12,33 @@ f.close()
 
 @app.route("/api/get", methods=["GET"])
 def handle_get_request():
-    habits = read_habits()
+    habits = read_habits('habits.json')
     return(habits)
 
-@app.route('/api', methods=['POST'])
+@app.route("/api_user2/get", methods=["GET"])
+def handle_get_request_2():
+    habits = read_habits('habits_user2.json')
+    return(habits)
+
+@app.route('/api/add', methods=['POST'])
 def handle_post_request():
     data = request.get_json()  # Parse the incoming JSON data
     if not data:
         return jsonify({"error": "No data provided"}), 400
     
     data = data.get('param', None)
-    print(data)
-    write_habit(data)
+    write_habit(data, 'habits.json')
+
+    return jsonify({"success": "probably good!"}), 200
+
+@app.route('/api_user2/add', methods=['POST'])
+def handle_post_request_2():
+    data = request.get_json()  # Parse the incoming JSON data
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    
+    data = data.get('param', None)
+    write_habit(data, 'habits_user2.json')
 
     return jsonify({"success": "probably good!"}), 200
 
@@ -34,7 +49,19 @@ def handle_increase_xp():
     if not data or 'habit' not in data:
         return jsonify({"error": "Invalid request: 'habit' is required"}), 400
     try:
-        increase_xp(data['habit'])
+        increase_xp(data['habit'], 'habits.json')
+        return jsonify({"success": f"XP increased for habit '{data['habit']}'"}), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 404
+    
+@app.route('/api_user2/increase_xp', methods=['POST'])
+def handle_increase_xp_2():
+    data = request.get_json()  # Parse the incoming JSON data
+    data = data.get('param', None)
+    if not data or 'habit' not in data:
+        return jsonify({"error": "Invalid request: 'habit' is required"}), 400
+    try:
+        increase_xp(data['habit'], 'habits_user2.json')
         return jsonify({"success": f"XP increased for habit '{data['habit']}'"}), 200
     except ValueError as e:
         return jsonify({"error": str(e)}), 404
